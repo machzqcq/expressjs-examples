ebc05cd5802a2be36c51d378e1349b19455eec02
Pradeep <pradeep@seleniumframework.com>
2016-06-19 22:40:09 -0400

    using connect Static (seems connect.static is deprecated)

---
+++ connect/middlewareRouteStatic.js
@@ -0,0 +1,8 @@
+
+var connect = require('connect');
+
+var server = connect()
+	.use(connect.static(__dirname + '/public'))
+	.use(function onRequest(request, response) {
+		response.end('Hello from Connect!\n');
+	}).listen(3000);
